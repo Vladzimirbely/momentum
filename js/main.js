f3cbe259a1b3b0633da0d8d20974589d6fc5e5cb
VladzimirBely <vladzimir.bely@gmail.com>
2022-10-24 17:14:00 +0300

    create greeting

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 const time = document.querySelector('.time');
 const dateToday = document.querySelector('.date');
+const date = new Date();
+const userName = document.querySelector('.name')
 
 class Time {
-    constructor() {
-    }
-
     getTime() {
         return new Date().toLocaleTimeString();
      }
@@ -16,15 +15,52 @@ class Time {
     }
 
     getDate() {
-        const date = new Date();
         const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        const dateDays = date.getDay();
+        const currentDay = date.getDay();
         const options = {day: 'numeric', month: 'long', timeZone: 'UTC'};
         const currentDate = date.toLocaleDateString('en-En', options);
-        dateToday.textContent = `${days[dateDays]}, ${currentDate}`;
+        dateToday.textContent = `${days[currentDay]}, ${currentDate}`;
     }
 }
 
-let timeZone = new Time();
+const timeZone = new Time();
 timeZone.showTime();
 timeZone.getDate();
+
+class Greetings {
+    getTimeOfDay() {
+        const hours = date.getHours();
+        const greeting = document.querySelector('.greeting')
+        
+        if (hours >= 0) {
+            greeting.textContent = 'Good night';
+        }
+        if (hours >= 6) {
+            greeting.textContent = 'Good morning';
+        }
+        if (hours >= 12) {
+            greeting.textContent = 'Good afternoon';
+        }
+        if (hours >= 18) {
+            greeting.textContent = 'Good evening';
+        } 
+    }    
+}
+
+const greetings = new Greetings();
+greetings.getTimeOfDay();
+
+function setLocalStorage() {
+    localStorage.setItem('name', userName.value);
+  }
+
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+
+    if(localStorage.getItem('name')) {
+        userName.value = localStorage.getItem('name');
+    }
+  }
+
+window.addEventListener('load', getLocalStorage);
